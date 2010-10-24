be5579a93aed40ccb3c2adcd5a77576a85478136
Kenton Varda <temporal@gmail.com>
2010-10-23 23:35:55 -0700

    Introduce template class representing a table, indexed on multiple columns.  This will be used to replace the various ad-hoc bimaps in the Driver, which were really painful to keep straight.

---
+++ src/Table.h
@@ -0,0 +1,338 @@
+// ekam -- http://code.google.com/p/ekam
+// Copyright (c) 2010 Kenton Varda and contributors.  All rights reserved.
+// Portions copyright Google, Inc.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of the ekam project nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef EKAM_TABLE_H_
+#define EKAM_TABLE_H_
+
+#include <tr1/unordered_map>
+#include <vector>
+
+namespace ekam {
+
+// =======================================================================================
+// Internal helpers.  Please ignore.
+
+template <typename Key, typename Value>
+class DummyMap {
+public:
+  typedef std::pair<const Key, Value> value_type;
+  typedef value_type* iterator;
+  typedef const value_type* const_iterator;
+  inline iterator begin() { return NULL; }
+  inline iterator end() { return NULL; }
+  inline const_iterator begin() const { return NULL; }
+  inline const_iterator end() const { return NULL; }
+  inline iterator insert(const value_type& value) {
+    return NULL;
+  }
+  inline int erase(const iterator& iter) {
+    return 0;
+  }
+};
+
+template <typename Choices, int index>
+struct ChooseType;
+template <typename Choices>
+struct ChooseType<Choices, 0> : public Choices::Choice0 {};
+template <typename Choices>
+struct ChooseType<Choices, 1> : public Choices::Choice1 {};
+template <typename Choices>
+struct ChooseType<Choices, 2> : public Choices::Choice2 {};
+
+// =======================================================================================
+
+template <typename T, typename Hasher = std::tr1::hash<T>, typename Eq = std::equal_to<T> >
+struct IndexedColumn {
+  typedef T Value;
+  typedef std::tr1::unordered_multimap<T, int> Index;
+};
+
+template <typename T, typename Hasher = std::tr1::hash<T>, typename Eq = std::equal_to<T> >
+struct UniqueColumn {
+  typedef T Value;
+  typedef std::tr1::unordered_map<T, int> Index;
+};
+
+template <typename T, typename Hasher = std::tr1::hash<T>, typename Eq = std::equal_to<T> >
+struct Column {
+  typedef T Value;
+  typedef DummyMap<T, int> Index;
+};
+
+struct EmptyColumn {
+  struct Value {};
+  typedef DummyMap<Value, int> Index;
+};
+
+template <typename Column0, typename Column1 = EmptyColumn, typename Column2 = EmptyColumn>
+class Table {
+private:
+  struct Columns {
+#define CHOICE(INDEX) \
+    struct Choice##INDEX : public Column##INDEX { \
+      static inline typename Column##INDEX::Index* index(Table* table) { \
+        return &table->index##INDEX; \
+      } \
+      static inline const typename Column##INDEX::Index& index(const Table& table) { \
+        return table.index##INDEX; \
+      } \
+      template <typename Row> \
+      static inline const typename Column##INDEX::Value& cell(const Row& row) { \
+        return row.cell##INDEX; \
+      } \
+    };
+    CHOICE(0)
+    CHOICE(1)
+    CHOICE(2)
+#undef CHOICE
+  };
+  template <int columnNumber>
+  class Column : public ChooseType<Columns, columnNumber> {};
+
+public:
+  Table() : deletedCount(0) {}
+  ~Table() {}
+
+  class Row {
+  public:
+    inline Row() {}
+
+    template <int columnNumber>
+    const typename Column<columnNumber>::Value& cell() const {
+      return Column<columnNumber>::cell(*this);
+    }
+
+  private:
+    typename Column<0>::Value cell0;
+    typename Column<1>::Value cell1;
+    typename Column<2>::Value cell2;
+    bool deleted;
+
+    inline Row(const typename Column<0>::Value& cell0,
+               const typename Column<1>::Value& cell1,
+               const typename Column<2>::Value& cell2)
+      : cell0(cell0), cell1(cell1), cell2(cell2), deleted(false) {}
+
+    friend class Table;
+  };
+
+  class RowIterator {
+  public:
+    inline RowIterator() {}
+    RowIterator(const Table& table)
+        : current(NULL), nextIter(table.rows.begin()), end(table.rows.end()) {}
+
+    bool next() {
+      while (true) {
+        if (nextIter == end) {
+          return false;
+        } else if (!nextIter->deleted) {
+          current = &*nextIter;
+          ++nextIter;
+          return true;
+        }
+        ++nextIter;
+      }
+    }
+
+    inline const Row& row() {
+      return *current;
+    }
+
+  private:
+    const Row* current;
+    typename std::vector<Row>::const_iterator nextIter;
+    const typename std::vector<Row>::const_iterator end;
+  };
+
+  template <int columnNumber>
+  class SearchIterator {
+  public:
+    inline SearchIterator() {}
+    SearchIterator(const Table& table, const typename Column<columnNumber>::Value& value)
+        : table(table), current(NULL),
+          range(Column<columnNumber>::index(table).equal_range(value)) {}
+
+    inline bool next() {
+      while (true) {
+        if (range.first == range.second) {
+          return false;
+        }
+
+        const Row* row = &table.rows[range.first->second];
+
+        if (!row->deleted) {
+          current = row;
+          ++range.first;
+          return true;
+        }
+
+        ++range.first;
+      }
+    }
+
+    inline const Row& row() {
+      return *current;
+    }
+
+  private:
+    typedef typename Column<columnNumber>::Index::const_iterator InnerIter;
+    const Table& table;
+    const Row* current;
+    std::pair<InnerIter, InnerIter> range;
+  };
+
+  template <int columnNumber>
+  const Row* find(const typename Column<columnNumber>::Value& value) const {
+    typename Column<columnNumber>::Index::const_iterator iter =
+        Column<columnNumber>::index(*this).find(value);
+    if (iter == Column<columnNumber>::index(*this).end()) {
+      return NULL;
+    } else {
+      const Row* row = &rows[iter->second];
+      return row->deleted ? NULL : row;
+    }
+  }
+
+  template <int columnNumber>
+  const int erase(const typename Column<columnNumber>::Value& value) {
+    typedef typename Column<columnNumber>::Index::iterator ColumnIterator;
+    std::pair<ColumnIterator, ColumnIterator> range =
+        Column<columnNumber>::index(this)->equal_range(value);
+
+    int count = 0;
+    for (ColumnIterator iter = range.first; iter != range.second; ++iter) {
+      if (!rows[iter->second].deleted) {
+        rows[iter->second].deleted = true;
+        ++count;
+      }
+    }
+
+    deletedCount += count;
+    if (deletedCount >= 16 && deletedCount > rows.size() / 2) {
+      refresh();
+    } else {
+      // This isn't strictly necessary, but since it's convenient, let's delete the range
+      // from the index.
+      Column<columnNumber>::index(this)->erase(range.first, range.second);
+    }
+
+    return count;
+  }
+
+  void add(const typename Column<0>::Value& value0,
+           const typename Column<1>::Value& value1 = EmptyColumn::Value(),
+           const typename Column<2>::Value& value2 = EmptyColumn::Value()) {
+    handleInsertResult(index0.insert(typename Column<0>::Index::value_type(value0, rows.size())));
+    handleInsertResult(index1.insert(typename Column<1>::Index::value_type(value1, rows.size())));
+    handleInsertResult(index2.insert(typename Column<2>::Index::value_type(value2, rows.size())));
+    rows.push_back(Row(value0, value1, value2));
+  }
+
+  int size() {
+    return rows.size() - deletedCount;
+  }
+  int capacity() {
+    return rows.capacity();
+  }
+
+  template <int columnNumber>
+  int indexSize() {
+    return Column<columnNumber>::index(this)->size();
+  }
+
+private:
+  std::vector<Row> rows;
+  size_t deletedCount;
+
+  typename Column<0>::Index index0;
+  typename Column<1>::Index index1;
+  typename Column<2>::Index index2;
+
+  void refresh() {
+    std::vector<int> newLocations;
+    newLocations.reserve(rows.size());
+
+    {
+      std::vector<Row> newRows;
+      newRows.reserve(rows.size() - deletedCount);
+
+      for (size_t i = 0; i < rows.size(); i++) {
+        if (rows[i].deleted) {
+          newLocations.push_back(-1);
+        } else {
+          newLocations.push_back(newRows.size());
+          newRows.push_back(rows[i]);
+        }
+      }
+
+      rows.swap(newRows);
+      deletedCount = 0;
+    }
+
+    refreshColumn<0>(newLocations);
+    refreshColumn<1>(newLocations);
+    refreshColumn<2>(newLocations);
+  }
+
+  template <int columnNumber>
+  void refreshColumn(const std::vector<int>& newLocations) {
+    typedef Column<columnNumber> C;
+    typedef typename C::Index::iterator ColumnIterator;
+    ColumnIterator iter = C::index(this)->begin();
+    while (iter != C::index(this)->end()) {
+      int newLocation = newLocations[iter->second];
+      if (newLocation == -1) {
+        ColumnIterator iter2 = iter;
+        ++iter2;
+        C::index(this)->erase(iter);
+        iter = iter2;
+      } else {
+        iter->second = newLocation;
+        ++iter;
+      }
+    }
+  }
+
+  template <typename Iterator>
+  void handleInsertResult(const std::pair<Iterator, bool>& result) {
+    if (!result.second) {
+      rows[result.first->second].deleted = true;
+      result.first->second = rows.size();
+    }
+  }
+
+  template <typename Iterator>
+  void handleInsertResult(const Iterator& iter) {}
+};
+
+}  // namespace ekam
+
+#endif  // EKAM_TABLE_H_
---
+++ src/Table_test.cpp
@@ -0,0 +1,245 @@
+// c++ -- http://code.google.com/p/c++
+// Copyright (c) 2010 Kenton Varda and contributors.  All rights reserved.
+// Portions copyright Google, Inc.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of the c++ project nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "Table.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <set>
+#include <map>
+#include <string>
+
+namespace ekam {
+namespace {
+
+#define ASSERT(EXPRESSION)                                                    \
+  if (!(EXPRESSION)) {                                                        \
+    fprintf(stderr, "%s:%d: FAILED: %s\n", __FILE__, __LINE__, #EXPRESSION);  \
+    exit(1);                                                                  \
+  }
+
+void testTable() {
+  {
+    typedef Table<IndexedColumn<int> > MyTable;
+    MyTable table;
+
+    table.add(1234);
+    table.add(5678);
+
+    const MyTable::Row* row = table.find<0>(1234);
+    ASSERT(table.find<0>(4321) == NULL);
+    ASSERT(row != NULL);
+    ASSERT(row->cell<0>() == 1234);
+    row = table.find<0>(5678);
+    ASSERT(row != NULL);
+    ASSERT(row->cell<0>() == 5678);
+
+    {
+      std::multiset<int> values;
+      MyTable::RowIterator iter(table);
+
+      ASSERT(iter.next());
+      values.insert(iter.row().cell<0>());
+      ASSERT(iter.next());
+      values.insert(iter.row().cell<0>());
+      ASSERT(!iter.next());
+
+      ASSERT(values.count(1234) == 1);
+      ASSERT(values.count(5678) == 1);
+    }
+
+    table.erase<0>(1234);
+
+    ASSERT(table.find<0>(4321) == NULL);
+    ASSERT(table.find<0>(1234) == NULL);
+    row = table.find<0>(5678);
+    ASSERT(row != NULL);
+    ASSERT(row->cell<0>() == 5678);
+  }
+
+  {
+    typedef Table<UniqueColumn<int>, IndexedColumn<int> > MyTable;
+    MyTable table;
+
+    table.add(12, 34);
+    table.add(56, 34);
+
+    {
+      std::multiset<int> values;
+      MyTable::SearchIterator<1> iter(table, 34);
+
+      ASSERT(iter.next());
+      values.insert(iter.row().cell<0>());
+      ASSERT(iter.next());
+      values.insert(iter.row().cell<0>());
+      ASSERT(!iter.next());
+
+      ASSERT(values.count(12) == 1);
+      ASSERT(values.count(56) == 1);
+    }
+
+    table.add(12, 78);
+    const MyTable::Row* row = table.find<0>(12);
+    ASSERT(row != NULL);
+    ASSERT(row->cell<0>() == 12);
+    ASSERT(row->cell<1>() == 78);
+
+    {
+      MyTable::SearchIterator<1> iter(table, 34);
+
+      ASSERT(iter.next());
+      ASSERT(iter.row().cell<0>() == 56);
+      ASSERT(!iter.next());
+    }
+  }
+
+  {
+    typedef Table<IndexedColumn<std::string>, IndexedColumn<int>, Column<char> > MyTable;
+    MyTable table;
+
+    table.add("foo", 1, 'f');
+    table.add("foo", 2, 'o');
+    table.add("foo", 3, 'o');
+    table.add("bar", 1, 'b');
+    table.add("bar", 2, 'a');
+    table.add("bar", 3, 'r');
+
+    {
+      std::map<int, char> values;
+      MyTable::SearchIterator<0> iter(table, "bar");
+
+      ASSERT(iter.next());
+      ASSERT(iter.row().cell<0>() == "bar");
+      values[iter.row().cell<1>()] = iter.row().cell<2>();
+      ASSERT(iter.next());
+      ASSERT(iter.row().cell<0>() == "bar");
+      values[iter.row().cell<1>()] = iter.row().cell<2>();
+      ASSERT(iter.next());
+      ASSERT(iter.row().cell<0>() == "bar");
+      values[iter.row().cell<1>()] = iter.row().cell<2>();
+      ASSERT(!iter.next());
+
+      ASSERT(values[1] == 'b');
+      ASSERT(values[2] == 'a');
+      ASSERT(values[3] == 'r');
+    }
+
+    {
+      std::map<std::string, char> values;
+      MyTable::SearchIterator<1> iter(table, 2);
+
+      ASSERT(iter.next());
+      ASSERT(iter.row().cell<1>() == 2);
+      values[iter.row().cell<0>()] = iter.row().cell<2>();
+      ASSERT(iter.next());
+      ASSERT(iter.row().cell<1>() == 2);
+      values[iter.row().cell<0>()] = iter.row().cell<2>();
+      ASSERT(!iter.next());
+
+      ASSERT(values["foo"] == 'o');
+      ASSERT(values["bar"] == 'a');
+    }
+  }
+
+  {
+    typedef Table<IndexedColumn<int>, IndexedColumn<int> > MyTable;
+    MyTable table;
+
+    for (int i = 0; i < 50; i++) {
+      table.add(123, i);
+      table.add(456, i);
+      table.add(789, i);
+    }
+
+    ASSERT(table.size() == 150);
+    ASSERT(table.capacity() >= 150);
+    ASSERT(table.indexSize<0>() == 150);
+    ASSERT(table.indexSize<1>() == 150);
+    table.erase<0>(123);
+
+    ASSERT(table.size() == 100);
+    ASSERT(table.capacity() >= 150);
+    ASSERT(table.indexSize<0>() == 100);
+    ASSERT(table.indexSize<1>() == 150);
+    table.erase<0>(456);
+
+    ASSERT(table.size() == 50);
+    ASSERT(table.capacity() == 50);
+    ASSERT(table.indexSize<0>() == 50);
+    ASSERT(table.indexSize<1>() == 50);
+
+    const MyTable::Row* row = table.find<1>(5);
+    ASSERT(row != NULL);
+    ASSERT(row->cell<0>() == 789);
+
+    {
+      std::multiset<int> values;
+      MyTable::SearchIterator<0> iter(table, 789);
+
+      for (int i = 0; i < 50; i++) {
+        ASSERT(iter.next());
+        values.insert(iter.row().cell<1>());
+      }
+      ASSERT(!iter.next());
+
+      for (int i = 0; i < 50; i++) {
+        ASSERT(values.count(i) == 1);
+      }
+    }
+
+    ASSERT(!MyTable::SearchIterator<0>(table, 123).next());
+    ASSERT(!MyTable::SearchIterator<0>(table, 456).next());
+
+    {
+      std::multiset<int> values0;
+      std::multiset<int> values1;
+      MyTable::RowIterator iter(table);
+
+      for (int i = 0; i < 50; i++) {
+        ASSERT(iter.next());
+        values0.insert(iter.row().cell<0>());
+        values1.insert(iter.row().cell<1>());
+      }
+      ASSERT(!iter.next());
+
+      ASSERT(values0.count(789) == 50);
+      for (int i = 0; i < 50; i++) {
+        ASSERT(values1.count(i) == 1);
+      }
+    }
+  }
+}
+
+}  // namespace
+}  // namespace ekam
+
+int main(int argc, char* argv[]) {
+  ekam::testTable();
+  return 0;
+}
